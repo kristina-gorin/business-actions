42f1b1c8119b724d5218e5fe9ed7841650c80630
Kristina Gorina <kristina.gorina@clover.com>
2023-05-04 15:07:29 -0400

    remove fail test

--- src/App.test.js
+++ src/App.test.js
@@ -11,6 +11,4 @@ it('should render', () => {
 
   expect(screen.getByText('Business Contacts')).toBeInTheDocument();
 });
-it('should do something amazing', () => {
-  expect(true).toBe(false);
-});
+
